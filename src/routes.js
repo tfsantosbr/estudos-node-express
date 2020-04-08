42d5f7dfe6e06df836fa9ecb40450e0bb2478ec4
Tiago Santos <tfsantosbr@gmail.com>
2020-04-07 22:41:50 -0300

    upload de arquivo

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import AccountController from './app/controllers/AccountController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/account', AccountController.create);
 routes.post('/account/login', AccountController.login);
@@ -12,5 +15,9 @@ routes.post('/account/login', AccountController.login);
 routes.use(authMiddleware);
 
 routes.put('/account', AccountController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
